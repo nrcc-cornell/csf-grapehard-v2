68143cf3aae1d00ada0bde55368ca2635f33fb98
Ben <benlinux915@gmail.com>
2022-06-17 13:04:48 -0400

    data feetching and processing complete

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+import getData from './Scripts/getData';
+
 
 function App() {
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const newData = await getData([-72.8307,41.8098], '2021-03-16', [1000, 1100], 43);
+      setData(newData);
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <div>React App</div>
+    <pre>{JSON.stringify(data, null, 2)}</pre>
   );
 }
 
