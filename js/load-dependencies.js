9894e78ffc1cf921a71a872a5fed79f7cf7d7022
Ben <benlinux915@gmail.com>
2022-08-19 13:47:29 -0400

    Updates

---
+++ js/load-dependencies.js
@@ -0,0 +1,15 @@
+function loadToolDependencies() {
+
+  var dependency;
+  var element = document.getElementsByTagName('body')[0];
+
+  console.log('loading CSF-GRAPEHARD css');
+  dependency = document.createElement('link');
+  dependency.setAttribute('rel','stylesheet');
+  dependency.setAttribute('type','text/css');
+  // eslint-disable-next-line no-undef
+  dependency.setAttribute('href', CSFTOOL_URL + '/style/csf-grapehard-v2.css?v=4.0.0');
+  element.appendChild(dependency);
+
+}
+loadToolDependencies();
